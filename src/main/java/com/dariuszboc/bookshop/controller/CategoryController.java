8ef6f334c10414200c8383852853b1d1da0b7a5f
bocdarek <bocdarek@gmail.com>
2023-01-22 00:49:08 +0100

    categories completed

--- src/main/java/com/dariuszboc/bookshop/controller/CategoryController.java
+++ src/main/java/com/dariuszboc/bookshop/controller/CategoryController.java
@@ -5,13 +5,11 @@ import com.dariuszboc.bookshop.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("categories")
 public class CategoryController {
 
     private final CategoryRepository categoryRepository;
@@ -21,10 +19,36 @@ public class CategoryController {
         this.categoryRepository = categoryRepository;
     }
 
-    @GetMapping("/")
-    public String listCategories(Model model) {
+    @GetMapping("/categories")
+    public String categoryList(Model model) {
         List<Category> categories = categoryRepository.findAll();
         model.addAttribute("categories", categories);
         return "categories/category-list";
+    }
+
+    @GetMapping("/categories/addCategoryForm")
+    public String addCategoryForm(Model model) {
+        Category category = new Category();
+        model.addAttribute("category", category);
+        return "categories/add-category-form";
+    }
+
+    @PostMapping("/categories/saveCategory")
+    public String saveCategory(@ModelAttribute Category category) {
+        categoryRepository.save(category);
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/showUpdateForm")
+    public String showUpdateForm(@RequestParam Long id, Model model) {
+        Category category = categoryRepository.findById(id).get();
+        model.addAttribute("category", category);
+        return "categories/add-category-form";
+    }
+
+    @GetMapping("/categories/deleteCategory")
+    public String deleteCategory(@RequestParam Long id) {
+        categoryRepository.deleteById(id);
+        return "redirect:/categories";
     }
 }
