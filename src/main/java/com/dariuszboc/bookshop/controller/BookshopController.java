158f07f10d1408aa668e98cb94b40c837c796a0e
bocdarek <bocdarek@gmail.com>
2023-01-22 11:15:17 +0100

    navbar finished

--- src/main/java/com/dariuszboc/bookshop/controller/BookshopController.java
+++ src/main/java/com/dariuszboc/bookshop/controller/BookshopController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookshopController {
 
-    @GetMapping("/")
+    @GetMapping(value={"", "/", "index"})
     public String showBookshop() {
         return "index";
     }
