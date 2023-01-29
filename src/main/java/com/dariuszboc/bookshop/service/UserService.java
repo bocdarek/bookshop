9a33b39d4c6e1386179d3d62869bb983f76a17b0
bocdarek <bocdarek@gmail.com>
2023-01-29 11:08:56 +0100

    added user service

--- src/main/java/com/dariuszboc/bookshop/service/UserService.java
+++ src/main/java/com/dariuszboc/bookshop/service/UserService.java
@@ -1,9 +1,94 @@
 package com.dariuszboc.bookshop.service;
 
+import com.dariuszboc.bookshop.DTO.UserDTO;
+import com.dariuszboc.bookshop.entity.User;
+import com.dariuszboc.bookshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<UserDTO> findAll() {
+        List<User> users = userRepository.findAll();
+        return getDtoListFromUserList(users);
+    }
+
+    public void save(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        user.setFullName(userDTO.getFullName());
+        user.setAddress(userDTO.getAddress());
+        user.setEmail(userDTO.getEmail());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+        user.setRole("USER");
+        userRepository.save(user);
+    }
+
+    public void saveWithNewPassword(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        user.setFullName(userDTO.getFullName());
+        user.setAddress(userDTO.getAddress());
+        user.setEmail(userDTO.getEmail());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+        user.setRole("USER");
+        userRepository.save(user);
+    }
+
+    public UserDTO findById(Long id) {
+        User user =  userRepository.findById(id).get();
+        return getUserDTOFromUser(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public UserDTO findByUsername(String name) {
+        User user = userRepository.findByUsername(name);
+        return getUserDTOFromUser(user);
+    }
+
+    private UserDTO getUserDTOFromUser(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setFullName(user.getFullName());
+        userDTO.setAddress(user.getAddress());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPhoneNumber(user.getPhoneNumber());
+        userDTO.setRole(userDTO.getRole());
+        return userDTO;
+    }
+
+
+    private List<UserDTO> getDtoListFromUserList(List<User> users) {
+        List<UserDTO> usersDTO = new ArrayList<>();
+        users.forEach(user -> {
+            Long id = user.getId();
+            UserDTO userDTO = this.findById(id);
+            usersDTO.add(userDTO);
+        });
+        return usersDTO;
+    }
 }
+
