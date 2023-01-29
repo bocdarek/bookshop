4e9c6ad73a0b493088aa526dcd33276190bb9b8b
bocdarek <bocdarek@gmail.com>
2023-01-29 13:42:08 +0100

    update userdata by user implementd

--- src/main/java/com/dariuszboc/bookshop/controller/UserController.java
+++ src/main/java/com/dariuszboc/bookshop/controller/UserController.java
@@ -4,6 +4,8 @@ import com.dariuszboc.bookshop.DTO.ProductDTO;
 import com.dariuszboc.bookshop.DTO.UserDTO;
 import com.dariuszboc.bookshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,6 +37,15 @@ public class UserController {
         return "account/my-account";
     }
 
+    @GetMapping("/myAccount/myData")
+    public String showMyAccountData(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+        UserDTO userDTO = userService.findByUsername(name);
+        model.addAttribute("userDTO", userDTO);
+        return "account/my-account-data";
+    }
+
     @GetMapping("/createAccount")
     public String createAccountForm(Model model) {
         UserDTO userDTO = new UserDTO();
@@ -46,6 +57,12 @@ public class UserController {
     public String saveUser(@ModelAttribute UserDTO userDTO) {
         userService.saveWithNewPassword(userDTO);
         return "redirect:/index";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute UserDTO userDTO) {
+        userService.save(userDTO);
+        return "redirect:/myAccount";
     }
 
     @GetMapping("/users/deleteUser")
